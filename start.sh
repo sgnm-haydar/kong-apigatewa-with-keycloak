6ef3f07a15939b737b882c5444aae72023e2ead4
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-04-29 11:25:57 +0300

    sh updated

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ docker-compose up -d konga
 docker-compose ps
 
 #connect oidc driver to kong
-curl -s -X POST http://localhost:8001/plugins \-d name=oidc \-d config.client_id=ifm_facility_client \-d config.client_secret=cBh97xovn2wC70DbnZfupiH4t1vQoDrU \-d config.bearer_only=yes \-d config.introspection_endpoint=http://172.19.100.120:8080/auth/realms/ifm/protocol/openid-connect/token/introspect \-d config.discovery=http://172.19.100.120:8080/auth/realms/ifm/.well-known/openid-configuration \ | python3 -mjson.tool
+curl -s -X POST http://localhost:8001/plugins \-d name=oidc \-d config.client_id=ifm_facility_client \-d config.client_secret=cBh97xovn2wC70DbnZfupiH4t1vQoDrU \-d config.bearer_only=yes \-d config.introspection_endpoint=http://172.30.99.121:8080/auth/realms/ifm/protocol/openid-connect/token/introspect \-d config.discovery=http://172.19.100.120:8080/auth/realms/ifm/.well-known/openid-configuration \ | python3 -mjson.tool
 
 #create service called facility
 curl -s -X POST http://localhost:8001/services -d name=facility -d url=http://10.250.2.72:3001/ | python3 -mjson.tool
